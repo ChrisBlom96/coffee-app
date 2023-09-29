dc22cb833094286ccbc0b13e394f5abf746fbd07
Sebastian Elstadt <elstadt.sebastian@gmail.com>
2023-09-29 14:08:50 +0200

    clean api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -14,62 +14,65 @@ export class ApiService {
   validateLogin(username: string, password: string): Observable<any> {
     const headers = new HttpHeaders()
       .set('Content-Type', 'text/xml');
+  
+    const body = `
+      <?xml version="1.0" encoding="utf-8"?>
+      <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                     xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                     xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+          <ValidateLogin xmlns="intelliacc.com/ws_Auth">
+            <InputUsername>${username}</InputUsername>
+            <InputPassword>${password}</InputPassword>
+          </ValidateLogin>
+        </soap:Body>
+      </soap:Envelope>
+    `.trim();
+  
+    return this.http.post(`${this.apiUrl}?op=Validate_Login`, body, { headers, responseType: 'text' });
+  }
 
+  getProducts(): Observable<any> {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'text/xml');
+  
     const body = `
     <?xml version="1.0" encoding="utf-8"?>
     <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
                    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
                    xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
       <soap:Body>
-        <ValidateLogin xmlns="intelliacc.com/ws_Auth">
-          <InputUsername>${username}</InputUsername>
-          <InputPassword>${password}</InputPassword>
-        </ValidateLogin>
+        <GetAllEntriesFromCOFFEESTOCK_TEMP_APPLICATION xmlns="intelliacc.com/ws_Products">
+          <Request></Request>
+        </GetAllEntriesFromCOFFEESTOCK_TEMP_APPLICATION>
       </soap:Body>
     </soap:Envelope>
     `.trim();
-
-    return this.http.post(`${this.apiUrl}?op=Validate_Login`, body, { headers, responseType: 'text' });
-  }
-  getProducts(): Observable<any> {
-    const headers = new HttpHeaders()
-      .set('Content-Type', 'text/xml');
-
-    const body = `
-    <?xml version="1.0" encoding="utf-8"?>
-    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                   xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-                   <soap:Body>
-                   <GetAllEntriesFromCOFFEESTOCK_TEMP_APPLICATION xmlns="intelliacc.com/ws_Products">
-                   </GetAllEntriesFromCOFFEESTOCK_TEMP_APPLICATION>
-                 </soap:Body>
-               </soap:Envelope>
-               `.trim();
-
+  
     return this.http.post(`${this.apiUrl}?op=GetAllEntriesFromCOFFEESTOCK_TEMP_APPLICATION`, body, { headers, responseType: 'text' });
   }
+
   savePod(pod: Pod, image?: string): Observable<any> {
     const headers = new HttpHeaders()
       .set('Content-Type', 'text/xml');
 
     const body = `
-    <?xml version="1.0" encoding="utf-8"?>
-    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                   xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-      <soap:Body>
-        <Save_Pod xmlns="intelliacc.com/ws_Products">
-          <Request>
-            <ProductNumber>${pod.productNumber}</ProductNumber>
-            <Name>${pod.name}</Name>
-            <PricePerBox>${pod.pricePerBox}</PricePerBox>
-            <PricePerUnit>${pod.pricePerUnit}</PricePerUnit>
-            ${image ? `<Image>${image}</Image>` : ''}
-          </Request>
-        </Save_Pod>
-      </soap:Body>
-    </soap:Envelope>
+      <?xml version="1.0" encoding="utf-8"?>
+      <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                     xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                     xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+          <Save_Pod xmlns="intelliacc.com/ws_Products">
+            <Request>
+              <ProductNumber>${pod.productNumber}</ProductNumber>
+              <Name>${pod.name}</Name>
+              <PricePerBox>${pod.pricePerBox}</PricePerBox>
+              <PricePerUnit>${pod.pricePerUnit}</PricePerUnit>
+              ${image ? `<Image>${image}</Image>` : ''}
+            </Request>
+          </Save_Pod>
+        </soap:Body>
+      </soap:Envelope>
     `.trim();
 
     return this.http.post(this.apiUrl, body, { headers, responseType: 'text' });
