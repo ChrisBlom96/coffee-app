a952adf77e26937c170cf91eb9dd8c4838b007fc
Sebastian Elstadt <elstadt.sebastian@gmail.com>
2023-09-29 11:31:58 +0200

    coffee flavours list and refresh

--- src/app/coffee-flavours/coffee-flavours.page.ts
+++ src/app/coffee-flavours/coffee-flavours.page.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ApiService } from '../services/api.service';
+
+interface Pod {
+  name: string;
+  productNumber: string;
+  photoUrl: string;
+}
 
 @Component({
   selector: 'app-coffee-flavours',
@@ -11,10 +18,62 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class CoffeeFlavoursPage implements OnInit {
+  pods: Pod[] = [{
+    name: 'Vanilla',
+    productNumber: 'CP001',
+    photoUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    name: 'Caramel',
+    productNumber: 'CP002',
+    photoUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    name: 'Hazelnut',
+    productNumber: 'CP003',
+    photoUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    name: 'Chocolate',
+    productNumber: 'CP004',
+    photoUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    name: 'Cinnamon',
+    productNumber: 'CP005',
+    photoUrl: 'https://via.placeholder.com/150'
+  }];
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.refresh();
   }
 
+  refresh(): void {
+    this.apiService.getProducts().subscribe((response: string) => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(response, 'text/xml');
+      const pods = Array.from(xml.getElementsByTagName('Product')).map((product: Element) => {
+        return {
+          name: product.getElementsByTagName('ProductName')[0]?.textContent || '',
+          productNumber: product.getElementsByTagName('ProductNumber')[0]?.textContent || '',
+          photoUrl: product.getElementsByTagName('PhotoUrl')[0]?.textContent || ''
+        };
+      });
+      this.pods = pods;
+    });
+  }
+
+  add(): void {
+    // TODO: Implement add functionality
+  }
+
+  edit(pod: Pod): void {
+    // TODO: Implement edit functionality
+  }
+
+  delete(pod: Pod): void {
+    // TODO: Implement delete functionality
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -33,4 +33,24 @@ export class ApiService {
 
     return this.http.post(this.apiUrl, body, { headers, responseType: 'text' });
   }
+  getProducts(): Observable<any> {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'text/xml')
+      .set('SOAPAction', 'http://tempuri.org/Get_Products');
+  
+    const body = `
+    <?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                   xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <Get_Products xmlns="intelliacc.com/ws_Products">
+          <Request></Request>
+        </Get_Products>
+      </soap:Body>
+    </soap:Envelope>
+    `.trim();
+  
+    return this.http.post(`${this.apiUrl}?op=Get_Products`, body, { headers, responseType: 'text' });
+  }
 }
